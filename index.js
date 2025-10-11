3c01d94d768d3978dded976b446e221ab0410d6d
laizu01993 <laizuaktar6@gmail.com>
2025-10-12 03:12:19 +0600

    add /menu GET route to fetch all menu items from MongoDB

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const menuCollection = client.db('bistroDB').collection('menu');
+
+        // to read menu collection
+        app.get('/menu', async(req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");

9aaddd5335c88dfaf2cd8764ef2504396bc8bee3
laizu01993 <laizuaktar6@gmail.com>
2025-11-06 02:20:07 +0600

    add Stripe payment intent endpoint and setup for payment recording

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
@@ -212,6 +213,22 @@ async function run() {
             res.send(result);
         })
 
+        // Payment Intent
+        app.post("/create-payment-intent", async (req, res) => {
+            // price in dollars
+            const { price } = req.body;
+            // convert to cents
+            const amount = parseInt(price * 100);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
